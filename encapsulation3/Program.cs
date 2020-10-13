949e525c89f253cb791b24406a4ff5f1a57b6885
lane froehlich <elaine.froehlich@gmail.com>
2020-10-12 21:39:14 -0400

    adding all files to repo.

---
+++ encapsulation3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOPpmfsm
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var worker = new FactoryWorker();
+            var result = worker.AddNumbers(2, 3);
+            Console.WriteLine(result.ToString());
+        }
+
+        public class FactoryWorker
+        {
+            //public FactoryWorker()
+            //{
+
+            //}
+            public int AddNumbers(int num1, int num2)
+            {
+                var result = num1 + num2;
+                return result;
+            }
+
+        }
+
+    }
+}
